29941e0c6ed6c9f39327c3a25a692623bbf8008b
Cat-wang <1050688864@qq.com>
2020-01-21 09:35:23 +0800

    初次提交

---
+++ js/awards.js
@@ -0,0 +1,32 @@
+window.awards = [
+  {
+    award: '特等奖',
+    count: 1,
+    name: '特等奖',
+    url: ''
+  },
+  {
+    award: '一等奖',
+    count: 2,
+    name: '一等奖',
+    url: ''
+  },
+  {
+    award: '二等奖',
+    count: 5,
+    name: '二等奖',
+    url: ''
+  },
+  {
+    award: '三等奖',
+    count: 20,
+    name: '三等奖',
+    url: ''
+  },
+  {
+    award: '鸿运奖',
+    count: 22,
+    name: '鸿运奖',
+    url: ''
+  },
+]
